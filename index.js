79c96821e660621f5fc8b540eb0fb53e5fcdc8b3
Filip Jirsák <filip@jirsak.org>
2023-03-26 23:04:14 +0200

    Příklad možného řešení

--- index.js
+++ index.js
@@ -1,32 +1,6 @@
-const renderSection = (emails, element) => {
-  element.innerHTML = emails
-    .map((email) => {
-      let iconClass = 'opened';
-      if (email.unread) {
-        iconClass = 'closed';
-      };
+import { EmailSection } from "./EmailSection/index.js";
 
-      return `
-        <div class="email">
-          <div class="email__head">
-            <button class="email__icon email__icon--${iconClass}"></button>
-            <div class="email__info">
-              <div class="email__sender">${email.sender.name}</div>
-              <div class="email__subject">${email.subject}</div>
-            </div>
-            <div class="email__time">${email.time}</div>
-          </div>
-          <div class="email__body"></div>
-        </div>
-      `;
-    })
-    .join('');
-};
-
-fetch(`https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=unread`)
-  .then((response) => response.json())
-  .then((data) => renderSection(data.emails, document.getElementById('unread')));
-
-fetch(`https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=read`)
-  .then((response) => response.json())
-  .then((data) => renderSection(data.emails, document.getElementById('read')));
+document.getElementById('app').append(
+  EmailSection({ heading: 'Nepřečtené', folder: 'unread' }),
+  EmailSection({ heading: 'Přečtené', folder: 'read' }),
+);
